2e63df30cbb4c7742384d4dbafba33c04d9586ef
joelthobias <joelthobias447@gmail.com>
2024-03-14 15:36:12 +0530

    14-03-2024 admin panel update

--- src/pages/admin-Dash/index.jsx
+++ src/pages/admin-Dash/index.jsx
@@ -2,39 +2,45 @@ import React, { useEffect, useState } from 'react';
 import './index.css'; // Import your CSS file
 
 const AdminDash = () => {
-  const [cart, setCart] = useState({});
-  const [productsData, setProductsData] = useState([]);
+  // const [cart, setCart] = useState({});
+  // const [productsData, setProductsData] = useState([]);
 
-  useEffect(() => {
-    const fetchCartData = async () => {
-      const cartUrl = `https://fakestoreapi.com/carts/5`;
-      try {
-        const response = await fetch(cartUrl);
-        const data = await response.json();
-        if (data.products) {
-          const productIds = data.products.map(product => product.productId);
+  // useEffect(() => {
+  //   const fetchCartData = async () => {
+  //     const cartUrl = `https://fakestoreapi.com/carts/5`;
+  //     try {
+  //       const response = await fetch(cartUrl);
+  //       const data = await response.json();
+  //       if (data.products) {
+  //         const productIds = data.products.map(product => product.productId);
 
-          const productDetailsPromises = productIds.map(productId => {
-            return fetch(`https://fakestoreapi.com/products/${productId}`)
-              .then(res => res.json());
-          });
+  //         const productDetailsPromises = productIds.map(productId => {
+  //           return fetch(`https://fakestoreapi.com/products/${productId}`)
+  //             .then(res => res.json());
+  //         });
 
-          const products = await Promise.all(productDetailsPromises);
-          setProductsData(products);
-        }
+  //         const products = await Promise.all(productDetailsPromises);
+  //         setProductsData(products);
+  //       }
 
-        setCart(data);
-      } catch (error) {
-        console.error(error);
-        throw error;
-      }
-    };
+  //       setCart(data);
+  //     } catch (error) {
+  //       console.error(error);
+  //       throw error;
+  //     }
+  //   };
 
-    fetchCartData();
-  }, []);
+  //   fetchCartData();
+  // }, []);
 
   return (
-    <h3>Admin Dashboard</h3>
+    <div className="admin-panel">
+
+      <h3>Admin Dashboard</h3>
+      <button onClick={()=>window.location.href="/Admin/add_product"} className="add-product">ADD Product</button>
+      <button onClick={()=>window.location.href="/Admin/view_products"} className="view-products">View Products</button>
+      <button onClick={()=>window.location.href="/Admin/view-orders"} className="view-orders">View Orders</button>
+    </div>
   );
 };
 
