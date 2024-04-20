d80df7415695bdef01fb45402f6facf9fcd5c078
joel <joelthobias447@gmail.com>
2024-04-20 08:37:07 +0530

    Updates

--- src/Components/cartCount.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const cartCount = () => {
-  return (
-    <div>cartCount</div>
-  )
-}
-
-export default cartCount
--- src/Components/port.js
+++
@@ -1,2 +0,0 @@
-export const port = 'https://majorproject-bwa5.onrender.com/';
-// export const port = 'http://localhost:4040';
